d6863b466f9c23a7b6dabac6559976d23053042b
Daniel Nguyen <danielhuannguyen@gmail.com>
2017-05-06 21:22:15 -0500

    Setup files for testing framework

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const Car = require("../index.js");
+const expect = chai.expect
+
+describe("Canary", function () {
+    it("1 should equal 1", function () {
+        expect(1).to.be.equal(1);
+    })
+});
