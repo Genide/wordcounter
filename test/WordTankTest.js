49f2551e8bfb21c5ea40ae0987e69a72171b6b32
Daniel Nguyen <danielhuannguyen@gmail.com>
2017-05-07 00:14:44 -0500

    Remove lambdas from tests

--- test/WordTankTest.js
+++ test/WordTankTest.js
@@ -14,17 +14,17 @@ describe("WordTank", function () {
             expect(wordTank.index).to.deep.eq({});
         })
 
-        it("Word array specified", () => {
+        it("Word array specified", function () {
             var wordTank = new WordTank(["hello"]);
             expect(wordTank.index).to.deep.eq({ "hello": 1 });
         })
 
-        it("Invalid object inside word array", () => {
+        it("Invalid object inside word array", function () {
             var wordTank = new WordTank([{ foo: "bar" }, "hello"]);
             expect(wordTank.index).to.deep.eq({ "hello": 1 });
         })
 
-        it("Invalid parameter", () => {
+        it("Invalid parameter", function () {
             var wordTank = new WordTank({ foo: "bar" });
             expect(wordTank.index).to.deep.eq({});
         })
@@ -32,46 +32,46 @@ describe("WordTank", function () {
 
     describe("indexWord", function () {
         var wordTank;
-        before("Create word tank instance", () => {
+        before("Create word tank instance", function () {
             wordTank = new WordTank();
         })
 
-        it("Index valid word", () => {
+        it("Index valid word", function () {
             wordTank.indexWord("hello");
             expect(wordTank.index).to.deep.eq({ "hello": 1 });
         })
 
-        it("Index invalid word", () => {
+        it("Index invalid word", function () {
             wordTank.indexWord(12345678);
             expect(wordTank.index).to.deep.eq({ "hello": 1 });
         })
 
-        it('Index empty string', () => {
+        it('Index empty string', function () {
             wordTank.indexWord("");
             expect(wordTank.index).to.deep.eq({ "hello": 1 });
         })
 
-        it('Index existing word again', () => {
+        it('Index existing word again', function () {
             wordTank.indexWord("hello");
             expect(wordTank.index).to.deep.eq({ "hello": 2 });
         })
     })
 
-    describe("getWordCount", () => {
+    describe("getWordCount", function () {
         var wordTank;
-        before("Create word tank instance", () => {
+        before("Create word tank instance", function () {
             wordTank = new WordTank(["hello", "world"]);
         })
 
-        it('Get valid word', () => {
+        it('Get valid word', function () {
             expect(wordTank.getWordCount("hello")).to.eq(1);
         })
 
-        it('Get missing word', () => {
+        it('Get missing word', function () {
             expect(wordTank.getWordCount("foo")).to.eq(0);
         })
 
-        it('Invalid parameter', () => {
+        it('Invalid parameter', function () {
             expect(wordTank.getWordCount({})).to.eq(0);
         })
     })
