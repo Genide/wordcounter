9545590692054d48c82035b59d4c4f7be0f09578
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2017-05-14 11:46:50 -0500

    Added file specific linting in dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,38 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 const exec = require('child_process').exec;
 const eslint = require('gulp-eslint');
+const changedInPlace = require('gulp-changed-in-place');
 
-gulp.task('js-watch', ['test'], function (done) {
+var swallowError = function (error) {
+    // console.log(error.name);
+    // console.log(error.message);
+    this.emit('end');
+}
+
+gulp.task('reload', function (done) {
     browserSync.reload();
     done();
 });
 
-gulp.task('test', ['lint'], function (cb) {
-    exec('npm run localTest --silent', {maxBuffer: 1024 * 1024}, function (error, stdout, stderr) {
+gulp.task('ccView', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: "./coverage"
+        }
+    });
+    done();
+});
+
+gulp.task('test', function (cb) {
+    exec('npm run localTest --silent', { maxBuffer: 1024 * 1024 }, function (error, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(error);
     })
 });
 
-gulp.task('lint', () => {
-    return gulp.src(['**/*.js','!node_modules/**','!coverage/**'])
+gulp.task('lint all', () => {
+    return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**'])
         .pipe(eslint())
         // eslint.format() outputs the lint results to the console.
         .pipe(eslint.format())
@@ -26,13 +42,22 @@ gulp.task('lint', () => {
         .pipe(eslint.failAfterError());
 })
 
-gulp.task('default', ['test'], function () {
-    browserSync.init({
-        server: {
-            baseDir: "./coverage"
-        }
-    });
+gulp.task('lint-in-place', () => {
+    return gulp.src(['./src/*.js', './test/*.js'])
+        .pipe(changedInPlace())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+})
 
-    gulp.watch('test/*.js', ['js-watch'])
-    gulp.watch('src/*.js', ['js-watch'])
+gulp.task('watch', (done) => {
+    var watcher = gulp.watch(['./src/*.js', './test/*.js'], gulp.series(['lint-in-place', 'test', 'reload']));
+    watcher.on('change', (path, stats) => {
+        console.log(path + ' was changed');
+    });
+    // This is needed to keep the watcher going if there is an ESLint or test error
+    watcher.on('error', swallowError);
+    done();
 });
+
+gulp.task('default', gulp.series('lint all', 'test', 'ccView', 'watch'));
